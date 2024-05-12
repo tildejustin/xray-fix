5f7bca21a0af864a6012b130f7646c6226838cd3
tildejustin <spacepepper3.14@gmail.com>
2024-05-12 15:36:19 -0400

    sodium null check, fixes #1

--- src/main/java/dev/tildejustin/xrayfix/SodiumCompat.java
+++ src/main/java/dev/tildejustin/xrayfix/SodiumCompat.java
@@ -1,12 +1,14 @@
 package dev.tildejustin.xrayfix;
 
 import dev.tildejustin.xrayfix.mixin.accessor.SodiumWorldRendererAccessor;
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderContainer;
 import me.jellysquid.mods.sodium.client.world.WorldRendererExtended;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 
 public class SodiumCompat {
     public static boolean isEntityChunkBuilt(Entity entity) {
-        return !((SodiumWorldRendererAccessor) ((WorldRendererExtended) MinecraftClient.getInstance().worldRenderer).getSodiumWorldRenderer()).getChunkRenderManager().getRender(entity.chunkX, entity.chunkY, entity.chunkZ).isEmpty();
+        ChunkRenderContainer<?> chunkRenderContainer = ((SodiumWorldRendererAccessor) ((WorldRendererExtended) MinecraftClient.getInstance().worldRenderer).getSodiumWorldRenderer()).getChunkRenderManager().getRender(entity.chunkX, entity.chunkY, entity.chunkZ);
+        return chunkRenderContainer != null && !chunkRenderContainer.isEmpty();
     }
 }
